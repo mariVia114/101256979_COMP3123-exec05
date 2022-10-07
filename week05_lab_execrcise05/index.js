7bb0350298da472c74159003f002332f6d115214
Marie <marievianca.pagaduan@georgebrown.ca>
2022-10-07 00:35:33 -0400

    Lab five solution and files added

---
+++ week05_lab_execrcise05/index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const fs = require('fs')
+const userData = JSON.parse(fs.readFileSync(`${__dirname}/user.json`))
+
+
+/*
+- Create new html file name home.html 
+- add <h1> tag with message "Welcome to ExpressJs Tutorial"
+- Return home.html page to client
+*/
+router.get('/home', (req,res) => {
+  res.sendFile(`${__dirname}/home.html`);
+});
+
+/*
+- Return all details from user.json file to client as JSON format
+*/
+router.get('/profile', (req,res) => {
+      res.send(userData);
+    })
+  
+
+/*
+- Modify /login router to accept username and password as query string parameter
+- Read data from user.json file
+- If username and  passsword is valid then send resonse as below 
+    {
+        status: true,
+        message: "User Is valid"
+    }
+- If username is invalid then send response as below 
+    {
+        status: false,
+        message: "User Name is invalid"
+    }
+- If passsword is invalid then send response as below 
+    {
+        status: false,
+        message: "Password is invalid"
+    }
+*/
+router.get('/login', (req,res) => {
+  let username = req.query.username
+  let password = req.query.password
+  let authorisedUser = userData.username
+  let savedPwd = userData.password
+
+  if(username == authorisedUser && password == savedPwd){
+    res.json({status:true, message:"User Is valid"});
+  }else if(username != authorisedUser){
+    res.json({status:false, message:"User is invalid"});
+  }else if(password != savedPwd){
+    res.json({status:false, message:"Password is invalid"});
+  }
+
+
+});
+
+/*
+- Modify /logout route to accept username as parameter and display message
+    in HTML format like <b>${username} successfully logout.<b>
+*/
+router.get('/logout', (req,res) => {
+  let username = req.query.username
+  res.send(`<b>${username} successfully logout</b>`);
+});
+
+app.use('/', router);
+
+app.listen(process.env.port || 8081);
+
+console.log('Web Server is listening at port '+ (process.env.port || 8081));
